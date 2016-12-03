e59fddb7ca6cab3bf1d6fbeecbd5030ab5d1d9c0
MarkSchipper <w.g.a.schipper@saxion.nl>
2016-12-03 14:20:10 +0100

    part 6

---
+++ LevelDesign/Assets/Editor/HotKeyEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Hotkeys))]
+public class HotKeyEditor : Editor {
+
+    
+    public override void OnInspectorGUI()
+    {
+
+        DrawDefaultInspector();
+        
+        if(GUILayout.Button("Save"))
+        {
+            Hotkeys _hotkey = (Hotkeys)target;
+            _hotkey.ChangeKeys();
+        }      
+        
+    }
+
+    
+
+
+}
